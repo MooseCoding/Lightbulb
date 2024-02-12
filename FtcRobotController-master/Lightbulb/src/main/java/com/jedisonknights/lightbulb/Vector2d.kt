44ff8341722ea5f1346d735c7fbafe8bfc746058
MooseCoding <113293872+MooseCoding@users.noreply.github.com>
2024-02-12 13:24:30 -0500

    Update Vector2d.kt

--- FtcRobotController-master/Lightbulb/src/main/java/com/jedisonknights/lightbulb/Vector2d.kt
+++ FtcRobotController-master/Lightbulb/src/main/java/com/jedisonknights/lightbulb/Vector2d.kt
@@ -1,5 +1,17 @@
 package com.jedisonknights.lightbulb
 
 open class Vector2d(x : Int, y : Int, theta : Double) {
+  val x_pos = x 
+  val y_pos = y
+  val angle = theta
+
+  private fun equals(Vector2d vector) {
+    if (this.x_pos == vector.x_pos && this.y_pos == vector.y_pos && this.angle = vector.angle) {
+      return true
+    }
+    return false
+  }
+
+  
 }
 
