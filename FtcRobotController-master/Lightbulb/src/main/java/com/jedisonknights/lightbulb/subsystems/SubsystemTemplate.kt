6d15a0a57242fafbffb27f95bc88e3626670a95b
MooseCoding <113293872+MooseCoding@users.noreply.github.com>
2024-02-24 21:05:06 -0500

    goofing off on my xbox

---
+++ FtcRobotController-master/Lightbulb/src/main/java/com/jedisonknights/lightbulb/subsystems/SubsystemTemplate.kt
@@ -0,0 +1,8 @@
+package com.jedisonknights.lightbulb.subsystems
+
+import com.jedisonknights.lightbulb.work_in_progrses.BetterMotor
+//import servo class from work_in_progrses
+
+open class SubsystemTemplate() {//passes in list of sensors, servos, and motors to use
+    
+}
